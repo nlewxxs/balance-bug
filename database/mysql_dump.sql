ed2ed68de12862f97e9c4e8e52d98b51215feac3
Benedict Short <benedictnjshort@gmail.com>
2023-05-21 16:50:28 +0100

    working database schema initialization+connection

--- database/mysql_dump.sql
+++ database/mysql_dump.sql
@@ -1,7 +1,6 @@
---USE docker-entrypoint-initdb.d;
 CREATE table BotKey (
     ConnTime TIMESTAMP NOT NULL,
     SessionKey char(100) NOT NULL,
     BugId char(100) NOT NULL,
     PRIMARY KEY (ConnTime, SessionKey, BugId)
-);
+) ENGINE=InnoDB;
