254366420cf25184b520db5129b1459f367a5362
Nik <nik.lewis21@imperial.ac.uk>
2023-05-22 14:11:47 +0100

    Removed most of the drift

--- 6DOF/6dof_complementary/6dof_complementary.ino
+++ 6DOF/6dof_complementary/6dof_complementary.ino
@@ -25,7 +25,7 @@ void setup() {
   Wire.write(0x00);                  // placing 0 in the PWR_MGMT_1 register (wakes mpu)
   Wire.endTransmission(true);        //end the transmission
 
-  Serial.print("la mpu esta hablando con el monitor serial!!!");
+  Serial.println("la mpu esta hablando con el monitor serial!!!");
 
   /*
   // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
@@ -59,8 +59,8 @@ void loop() {
   AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
 
   // Calculating Roll and Pitch from the accelerometer data
-  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - 0.58; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
-  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) + 1.58; // AccErrorY ~(-1.58)
+  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorX; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
+  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorY; // AccErrorY ~(-1.58)
 
   // reading gyro data 
   previousTime = currentTime;                         // Previous time is stored before the actual time read
@@ -78,9 +78,9 @@ void loop() {
   GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
 
   // Correct the outputs with the calculated error values
-  GyroX = GyroX + 0.56; // GyroErrorX ~(-0.56)
-  GyroY = GyroY - 2; // GyroErrorY ~(2)
-  GyroZ = GyroZ + 0.79; // GyroErrorZ ~ (-0.8)
+  GyroX = GyroX - GyroErrorX; // GyroErrorX ~(-0.56)
+  GyroY = GyroY - GyroErrorY; // GyroErrorY ~(2)
+  GyroZ = GyroZ - GyroErrorZ; // GyroErrorZ ~ (-0.8)
 
   // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
   gyroAngleX = gyroAngleX + GyroX * elapsedTime; // deg/s * s = deg
