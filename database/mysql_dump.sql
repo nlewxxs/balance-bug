09a6bf6768097befca043e3c6a6ac6c6f165db27
Benedict Short <benedictnjshort@gmail.com>
2023-05-22 13:48:55 +0000

    working on test-case

--- database/mysql_dump.sql
+++ database/mysql_dump.sql
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 CREATE table BotKey (
     ConnTime TIMESTAMP NOT NULL,
     SessionKey char(100) NOT NULL,
     BugId char(100) NOT NULL,
     PRIMARY KEY (ConnTime, SessionKey, BugId)
 ) ENGINE=InnoDB;
-=======
---USE docker-entrypoint-initdb.d;
-CREATE table BotKey (
-    ConnTime TIMESTAMP NOT NULL,
-    SessionKey char(100) NOT NULL,
-    BugId char(100) NOT NULL,
-    PRIMARY KEY (ConnTime, SessionKey, BugId)
-);
->>>>>>> b49d1e4ef260c43a6960a1dda7a60156ca7f1dd8
