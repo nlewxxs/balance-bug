3f663f7b47a0be60f7a209952708c37f9fb2492f
Benedict Short <benedictnjshort@gmail.com>
2023-05-22 14:50:03 +0100

    new dump

--- database/mysql_dump.sql
+++ database/mysql_dump.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE table BotKey (
     ConnTime TIMESTAMP NOT NULL,
     SessionKey char(100) NOT NULL,
@@ -8,11 +7,3 @@ CREATE table BotKey (
 
 
 INSERT INTO `testapp`.`BotKey` (`ConnTime`, `SessionKey`, `BugId`) VALUES (CURRENT_TIMESTAMP, 'test', 'test');
-=======
-CREATE table BotKey (
-    ConnTime TIMESTAMP NOT NULL,
-    SessionKey char(100) NOT NULL,
-    BugId char(100) NOT NULL,
-    PRIMARY KEY (ConnTime, SessionKey, BugId)
-) ENGINE=InnoDB;
->>>>>>> 09a6bf6768097befca043e3c6a6ac6c6f165db27
