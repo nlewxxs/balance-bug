e6e608d341bdc720f7bbf621aebb284f8d532ca7
ahumayde <ahumayde@gmail.com>
2023-05-23 16:32:46 +0100

    improved how session list gets data

--- app/src/components/LoadSession.js
+++ app/src/components/LoadSession.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SessionList from './SessionList';
 
-function MainMenu() {
-    return (
-      <div className="selection">  
-        <h1>Select Session</h1>        
-        <SessionList />
-        <button className="create_btn">
-            <Link to="/Create">Create New Session</Link> 
-        </button>
-        <button className="back_btn">
-            <Link to="/">Back to Menu</Link> 
-        </button>
-      </div>
-    )
+function LoadSession() {
+  const [sessions] = useState([
+    {name: "bug1", id: "123", date: "2023-05-10 18:00:00.00"},
+    {name: "bug2", id: "403", date: "2023-05-11 18:00:00.00"},
+    {name: "bug3", id: "101", date: "2023-05-12 18:00:00.00"}
+  ]);
+
+  return (
+    <div className="selection">  
+      <h1>Select Session</h1>        
+      <SessionList sessions={sessions} />
+      <button className="create_btn">
+          <Link to="/Create">Create New Session</Link> 
+      </button>
+      <button className="back_btn">
+          <Link to="/">Back to Menu</Link> 
+      </button>
+    </div>
+  )
 }
 
-export default MainMenu; 
+export default LoadSession; 
--- app/src/components/SessionList.js
+++ app/src/components/SessionList.js
@@ -1,17 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const SessionList = (/*{ sessions }*/) => {
-  const [sessions, setSessions] = useState([
-    {name: "bug1", id: "123", date: "2023-05-10 18:00:00.00"},
-    {name: "bug2", id: "403", date: "2023-05-11 18:00:00.00"},
-    {name: "bug2", id: "403", date: "2023-05-11 18:00:00.00"},
-    {name: "bug3", id: "101", date: "2023-05-12 18:00:00.00"}
-  ]);
+const SessionList = (props) => {
+  const sessions=props.sessions;
+
   const navigate = useNavigate();
   
   const handleClick = (id) => {
-
     navigate(`/Display/`+id);
   };
 
