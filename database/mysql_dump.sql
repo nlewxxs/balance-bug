baa068e4692d71142046f121fab7a3e3a65b807b
Benedict Short <benedictnjshort@gmail.com>
2023-05-27 00:18:09 +0100

    fixing database naming syntax

--- database/mysql_dump.sql
+++ database/mysql_dump.sql
@@ -1,6 +1,6 @@
 CREATE table SessionList (
-    'TimeStamp' TIMESTAMP NOT NULL,
-    'BugName' char(100) NOT NULL,
-    'SessionId' char(100) NOT NULL,
-    PRIMARY KEY ('TimeStamp', 'BugName', 'SessionId')
+    `TimeStamp` TIMESTAMP NOT NULL,
+    `BugName` char(100) NOT NULL,
+    `SessionId` char(100) NOT NULL,
+    PRIMARY KEY (`TimeStamp`, `BugName`, `SessionId`)
 ) ENGINE=InnoDB;
