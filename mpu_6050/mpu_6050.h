ffc5c0cefaf5788f2cb0efe710d00ede12440fb2
Nik <nik.lewis21@imperial.ac.uk>
2023-05-26 11:17:50 +0100

    Filled in mpu_6050.cpp
    library good to go for testing.

--- mpu_6050/mpu_6050.h
+++ mpu_6050/mpu_6050.h
@@ -8,21 +8,20 @@ class mpu6050 {
     public: 
         void update();
         void calibrate();
-        uint8_t calibration_length;
     private: 
         double AccErrorX;
         double AccErrorY;
         double GyroErrorX;
         double GyroErrorY;
         double GyroErrorZ;
-        void getPitch();
-        void getRoll();
-        void getYaw();
-        void getRawXAccel();
-        void getRawYAccel();
-        void getRawXGyro();
-        void getRawYGyro();
-        void getRawZGyro();
+        double getPitch();
+        double getRoll();
+        double getYaw();
+        double getRawXAccel();
+        double getRawYAccel();
+        double getRawXGyro();
+        double getRawYGyro();
+        double getRawZGyro();
 };
 
 #endif
