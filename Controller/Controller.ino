41f349fe15590e64d58406ed4e41e103298c4f88
JJ <jjluhilson@gmail.com>
2023-05-27 01:18:02 +0100

    fixed overflow issues, implemented controller with the mpu class

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,12 +1,12 @@
 // #include <ArduinoEigenDense.h>
 // using namespace Eigen;
 #include <Wire.h>
-// #include <MPU6050.h>
+#include "mpu6050.h"
 
 // heading PD controller
-double Kp_heading = 2.0;
+double Kp_heading = 0.8;
 double Ki_heading = 0; 
-double Kd_heading = 0.75;
+double Kd_heading = 0.2;
 
 // position PD controller
 double Kp_position = 0.0005;
@@ -14,9 +14,9 @@ double Ki_position = 0;
 double Kd_position = 0.75;
 
 // balance PID controller
-double Kp_tilt = 175;
-double Ki_tilt = 5.0;
-double Kd_tilt = 8.5;
+double Kp_tilt = 5; //175
+double Ki_tilt = 0.75; //5
+double Kd_tilt = 1; //8.5
 double balanceCenter = 0; // whatever tilt value is balanced
 double P_bias, T_bias, H_bias = 0;
 
@@ -24,18 +24,19 @@ double H_derivative, H_out, P_derivative, P_out, T_derivative, T_out;
 double H_error[2], P_error[2], T_error[2] = {0, 0}; //stores current and previous value for derivative calculation
 double H_integral[2], P_integral[2], T_integral[2] = {0, 0}; //stores current and previous value for integral calculation
 double iteration_time;
-int oldMillis = 0;
+unsigned long oldMillis = 0;
 
 // sensor definitions
 const int mpu_addy = 0x68;
-double AccX, AccY, AccZ;
-double GyroX, GyroY, GyroZ;  
-double accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
-double roll, pitch, yaw;
-double AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
-double elapsedTime, currentTime, previousTime;
-int calibrate = 0;  // calibration counter
+// double AccX, AccY, AccZ;
+// double GyroX, GyroY, GyroZ;  
+// double accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
+// double roll, pitch, yaw;
+// double AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
+// double elapsedTime, currentTime, previousTime;
+// int calibrate = 0;  // calibration counter
 
+mpu6050 mpu = mpu6050();
 
 void setup() {
   Serial.begin(9600);
@@ -47,20 +48,33 @@ void setup() {
   // m2 << 9, 3, 2, 6, 4, 2, 6, 7, 3, 1, 4, 2;
   // MatrixXf outm = m1 * m2;
 
-  Wire.begin();                      
-  Wire.beginTransmission(mpu_addy);  
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // placing 0 in the PWR_MGMT_1 register (wakes mpu)
-  Wire.endTransmission(true);        //end the transmission
+  // Wire.begin();                      
+  // Wire.beginTransmission(mpu_addy);  
+  // Wire.write(0x6B);                  // Talk to the register 6B
+  // Wire.write(0x00);                  // placing 0 in the PWR_MGMT_1 register (wakes mpu)
+  // Wire.endTransmission(true);        //end the transmission
+  mpu.init();
 
-  calculate_IMU_error();
+  mpu.calibrate();
   delay(20);
 }
 
 void loop() {
-  iteration_time = (millis() - oldMillis);
-  iteration_time = iteration_time / 1000;
+  mpu.update();
+
+  // unsigned long iteration_time_temp = (millis() - oldMillis);
+  // iteration_time = iteration_time_temp / 1000;
+  double iteration_time = (millis() - oldMillis) / 1000.0;
+
+  
+  // Serial.print("Old millis: ");
+  // Serial.print(oldMillis);
+
+
   oldMillis = millis();
+  
+  // Serial.print(" Current millis: ");
+  // Serial.print(oldMillis);
 
   // position control first
   double positionReading = getPosition(); // example I used for the controller has position read from rotary encoders - this value is total distance travelled
@@ -74,17 +88,26 @@ void loop() {
   P_error[0] = P_error[1];
 
   // balance control
-  double tiltReading = getTiltReading();
+  double tiltReading = mpu.getPitch();
+  // double tiltReading = 1;
   T_error[1] = balanceCenter + P_out - tiltReading; // offset the tilt reading with the distance output to allow us to manipulate the position of the robot via tilt
   T_integral[1] = T_integral[0] + T_error[1] * iteration_time; // 1 is current, 0 is old
   T_derivative = (T_error[1] - T_error[0]) / iteration_time;
   T_out = Kp_tilt*T_error[1] + Ki_tilt*T_integral[1] + Kd_tilt*T_derivative + T_bias;
 
+  // Serial.print(" Tilt: ");
+  // Serial.print(tiltReading);
+  // Serial.print(" Tilt output: ");
+  // Serial.print(T_out);
+  // Serial.print(" Time: ");
+  // Serial.println(iteration_time);
+
+
   T_integral[0] = T_integral[1]; // time shift integral readings after out calculated
   T_error[0] = T_error[1];
 
   // heading control to offset the wheels for rotation
-  double headingReading = getHeading();
+  double headingReading = mpu.getYaw();
   H_error[1] = getHeadingSetpoint() - headingReading;
   H_integral[1] = H_integral[0] + H_error[1] * iteration_time; // 1 is current, 0 is old
   H_derivative = (H_error[1] - H_error[0]) / iteration_time;
@@ -97,36 +120,26 @@ void loop() {
   double leftWheelDrive = T_out - H_out; // voltage / pwm that will actually drive the wheels
   double rightWheelDrive = T_out + H_out;
 
-  // Serial.print("Currently at\nPosition: ");
+  // Serial.print("Currently at:  Position: ");
   // Serial.print(positionReading);
-  // Serial.print("| Tilt: ");
+  // Serial.print(" Tilt: ");
   // Serial.print(tiltReading);
-  // Serial.print("| Heading: ");
-  // Serial.println(headingReading);
-  // Serial.print("Control outputs\nPosition: ");
+  // Serial.print(" Heading: ");
+  // Serial.print(headingReading);
+  
+  // Serial.print("\tControl outputs  Position: ");
   // Serial.print(P_out);
   // Serial.print("| Tilt: ");
   // Serial.print(T_out);
   // Serial.print("| Heading: ");
-  // Serial.println(H_out);
+  // Serial.print(H_out);
 
-  // Serial.print("Wheel inputs: ");
-  // Serial.print(leftWheelDrive);
-  // Serial.print(" / ");
-  // Serial.println(rightWheelDrive);
-
-  Serial.print("Tilt: ");
-  Serial.print(tiltReading);
-  Serial.print(" Tilt output: ");
-  Serial.println(T_out);
-
+  Serial.print("\tWheel inputs: ");
+  Serial.print(leftWheelDrive);
+  Serial.print(" / ");
+  Serial.println(rightWheelDrive);
 
 }
-
-// double getTilt() {
-//   // tilt of inverted pendulum
-//   return 95.0;
-// }
 
 double getPosition() { //unsure of how this reading will work - needs to be 1D (as in just x)
   // could potentially have position just be a relative thing i.e. move forwards 1 / backwards 1 rather than move to position 23?
@@ -138,125 +151,9 @@ double getPosSetpoint() {
   return 0;
 }
 
-double getHeading() { // probably range from 0->2pi
-  return yaw;
-}
-
 double getHeadingSetpoint() {
   // output from the imaging program to tell us where to face
   return 0;
-}
-
-double getTiltReading() {
-  // reading accelerometer data
-  Wire.beginTransmission(mpu_addy);
-  Wire.write(0x3B);                     
-  Wire.endTransmission(false);          
-  Wire.requestFrom(mpu_addy, 6, true);
-
-  // normalising
-  // for a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
-
-  // Calculating Roll and Pitch from the accelerometer data
-  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorX;
-  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorY;
-
-  // reading gyro data 
-  previousTime = currentTime;                         
-  currentTime = millis();                             
-  elapsedTime = (currentTime - previousTime) / 1000;  
-
-  Wire.beginTransmission(mpu_addy);
-  Wire.write(0x43); 
-  Wire.endTransmission(false);
-  Wire.requestFrom(mpu_addy, 6, true); 
-
-  // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0; 
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
-
-  // Correct the outputs with the calculated error values
-  GyroX = GyroX - GyroErrorX; 
-  GyroY = GyroY - GyroErrorY; 
-  GyroZ = GyroZ - GyroErrorZ; 
-
-  gyroAngleX = gyroAngleX + GyroX * elapsedTime; 
-  gyroAngleY = gyroAngleY + GyroY * elapsedTime;
-  yaw =  yaw + GyroZ * elapsedTime;
-
-  // Complementary filter
-  roll = 0.98 * gyroAngleX + 0.02 * accAngleX;
-  pitch = 0.98 * gyroAngleY + 0.02 * accAngleY;
-  
-  // Print the values on the serial monitor
-  // Serial.print(roll);
-  // Serial.print("/");
-  // Serial.print(pitch);
-  // Serial.print("/");
-  // Serial.println(yaw);
-
-  return pitch;
-}
-
-void calculate_IMU_error() {
-
-  while (calibrate < 200) {
-    Wire.beginTransmission(mpu_addy);
-    Wire.write(0x3B);
-    Wire.endTransmission(false);
-    Wire.requestFrom(mpu_addy, 6, true);
-
-    AccX = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    AccY = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-
-    // Sum all readings
-    AccErrorX = AccErrorX + ((atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI));
-    AccErrorY = AccErrorY + ((atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI));
-    calibrate++;
-  }
-  
-  //average out over 200 cycles above
-  AccErrorX = AccErrorX / calibrate;
-  AccErrorY = AccErrorY / calibrate;
-  calibrate = 0;
-  
-  while (calibrate < 200) {
-
-    Wire.beginTransmission(mpu_addy);
-    Wire.write(0x43);
-    Wire.endTransmission(false);
-    Wire.requestFrom(mpu_addy, 6, true);
-
-    GyroX = Wire.read() << 8 | Wire.read();
-    GyroY = Wire.read() << 8 | Wire.read();
-    GyroZ = Wire.read() << 8 | Wire.read();
-
-    // Sum all readings
-    GyroErrorX = GyroErrorX + (GyroX / 131.0);
-    GyroErrorY = GyroErrorY + (GyroY / 131.0);
-    GyroErrorZ = GyroErrorZ + (GyroZ / 131.0);
-    calibrate++;
-  }
-
-  GyroErrorX = GyroErrorX / calibrate;
-  GyroErrorY = GyroErrorY / calibrate;
-  GyroErrorZ = GyroErrorZ / calibrate;
-
-  // Serial.print("AccErrorX: ");
-  // Serial.println(AccErrorX);
-  // Serial.print("AccErrorY: ");
-  // Serial.println(AccErrorY);
-  // Serial.print("GyroErrorX: ");
-  // Serial.println(GyroErrorX);
-  // Serial.print("GyroErrorY: ");
-  // Serial.println(GyroErrorY);
-  // Serial.print("GyroErrorZ: ");
-  // Serial.println(GyroErrorZ);
 }
 
 
--- Controller/mpu6050.cpp
+++ Controller/mpu6050.cpp
@@ -14,8 +14,13 @@ double pitch, roll, yaw;
 
 const int mpu_addr = 0x68;
 
-void mpu6050::init(){
+unsigned long previousTime, currentTime, elapsedTimeTemp = 0;
+double elapsedTime;
 
+double accAngleX, accAngleY;
+double gyroAngleX, gyroAngleY;
+
+void mpu6050::init(){
     Wire.begin();
     Wire.beginTransmission(mpu_addr);
     Wire.write(0x6B); // writing to PWR_MGMT_1 register
@@ -25,10 +30,9 @@ void mpu6050::init(){
 
 void mpu6050::calibrate() {
 
-    int c;
-    c = calibration_span;
+    int c = 0;
 
-    while (c < 200){
+    while (c < calibration_span) {
 
         Wire.beginTransmission(mpu_addr);
         Wire.write(0x3B);
@@ -46,37 +50,31 @@ void mpu6050::calibrate() {
 
     AccErrorX = AccErrorX / calibration_span;
     AccErrorY = AccErrorY / calibration_span;
+    c = 0;
 
-    c = calibration_span;
-
-    while (c < 200){
+    while (c < calibration_span) {
         
         Wire.beginTransmission(mpu_addr);
         Wire.write(0x43);
         Wire.endTransmission(false);
         Wire.requestFrom(mpu_addr, 6, 1);
+        GyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
+        GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+        GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
 
-        GyroX = Wire.read() << 8 | Wire.read();
-        GyroY = Wire.read() << 8 | Wire.read();
-        GyroZ = Wire.read() << 8 | Wire.read();
-
-        GyroErrorX = GyroErrorX + (GyroX / 131.0);
-        GyroErrorY = GyroErrorY + (GyroY / 131.0);
-        GyroErrorZ = GyroErrorZ + (GyroZ / 131.0);
+        GyroErrorX = GyroX + GyroErrorX; 
+        GyroErrorY = GyroY + GyroErrorY; 
+        GyroErrorZ = GyroZ + GyroErrorZ;
+        c++;
     }
 
     GyroErrorX = GyroErrorX / calibration_span;
     GyroErrorY = GyroErrorY / calibration_span;
     GyroErrorZ = GyroErrorZ / calibration_span;
-    c++;
+    // c++;
 }
 
 void mpu6050::update(){
-
-    double previousTime, currentTime, elapsedTime;
-
-    double accAngleX, accAngleY;
-    double gyroAngleX, gyroAngleY;
 
     Wire.beginTransmission(mpu_addr);
     Wire.write(0x3B);                     
@@ -94,9 +92,11 @@ void mpu6050::update(){
     accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorY;
 
     // reading gyro data 
-    previousTime = currentTime;                         
-    currentTime = millis();                             
-    elapsedTime = (currentTime - previousTime) / 1000;  
+    previousTime = currentTime;
+    currentTime = millis();
+    // elapsedTime = (currentTime - previousTime) / 1000;
+    elapsedTimeTemp = (currentTime - previousTime);
+    elapsedTime = elapsedTimeTemp / 1000.0;
 
     Wire.beginTransmission(mpu_addr);
     Wire.write(0x43); 
@@ -115,7 +115,7 @@ void mpu6050::update(){
 
     gyroAngleX = gyroAngleX + GyroX * elapsedTime; 
     gyroAngleY = gyroAngleY + GyroY * elapsedTime;
-    yaw =  yaw + GyroZ * elapsedTime;
+    yaw = yaw + GyroZ * elapsedTime;
 
     // Complementary filter
     roll = 0.98 * gyroAngleX + 0.02 * accAngleX;
