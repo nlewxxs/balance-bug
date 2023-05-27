1854adf86003bb573691b6b4c870a239f771bf86
ahumayde <ahumayde@gmail.com>
2023-05-28 00:01:58 +0100

    fixed double fetch

--- app/src/index.js
+++ app/src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App';
 import './index.css';
+import { useEffect } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render( <App /> );
