fe524e49a530200cf6677451917af2ae92abb293
Benedict Short <benedictnjshort@gmail.com>
2023-05-28 16:56:06 +0000

    separating key functions

---
+++ backend/api/Api.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+
+
+)
+
+var db *sql.DB
+var err error
+
+func SetupMySQL() {
+	// for local testing only:
+
+	db, err = sql.Open("mysql", "root:helloworld@tcp(172.17.0.1:3308)/testdb?timeout=5s")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	if err = db.Ping(); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	log.Println("connected to mysql")
+}
---
+++ backend/api/Api_test.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+
+	"github.com/gin-gonic/contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+
+// Setup Gin Routes
+func SetupRoutes() *gin.Engine {
+	// Use Gin as router
+	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	router.Use(cors.New(config))
+
+	// Set routes for API
+	router.GET("/Session/DisplayAll", DisplaySessionList)
+	router.GET("/Session/Create", CreateSession)
+	// router.GET("/Botkey/update/:id/:done", UpdateTodoItem)
+	// // router.GET("/item/delete/:id", DeleteTodoItem)
+
+	// Set up Gin Server
+	return router
+}
+
+// Perform Reuest
+// and return response
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+// Init Test Function
+func TestMain(t *testing.T) {
+	SetupMySQL()
+	router = SetupRoutes()
+}
+
