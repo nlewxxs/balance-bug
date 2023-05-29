4f8a814d2e10c8d9e144d0ec318bd6b6f0a27f71
Benedict Short <benedictnjshort@gmail.com>
2023-05-29 18:02:16 +0100

    updating database initialization dump

--- database/mysql_dump.sql
+++ database/mysql_dump.sql
@@ -4,3 +4,9 @@ CREATE table SessionList (
     `SessionId` char(100) NOT NULL,
     PRIMARY KEY (`TimeStamp`, `BugName`, `SessionId`)
 ) ENGINE=InnoDB;
+
+CREATE table BugInformation (
+    `BugName` char(100) NOT NULL,
+    `LastSeen` TIMESTAMP NOT NULL,
+    PRIMARY KEY (`BugName`, `LastSeen`)
+) ENGINE=InnoDB;
