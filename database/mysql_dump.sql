4e878cc02c400f73044d6f5f3285644e45e23d8b
ahumayde <ahumayde@gmail.com>
2023-05-31 17:42:16 +0000

    formatting

--- database/mysql_dump.sql
+++ database/mysql_dump.sql
@@ -1,13 +1,14 @@
 CREATE table SessionList (
-    `TimeStamp` TIMESTAMP NOT NULL,
-    `BugName` char(100) NOT NULL,
-    `SessionId` char(100) NOT NULL,
-    PRIMARY KEY (`TimeStamp`, `BugName`, `SessionId`)
+    `TimeStamp`   TIMESTAMP NOT NULL,
+    `BugName`     char(100) NOT NULL,
+    `SessionId`   char(100) NOT NULL,
+    `SessionName` char(100) NOT NULL,
+    PRIMARY KEY (`SessionId`)
 ) ENGINE=InnoDB;
 
 CREATE table BugInformation (
-    `BugId` char(100) NOT NULL,
-    `BugName` char(100) NOT NULL,
+    `BugId`    char(100) NOT NULL,
+    `BugName`  char(100) NOT NULL,
     `LastSeen` TIMESTAMP NOT NULL,
     PRIMARY KEY (`BugId`)
 ) ENGINE=InnoDB;
