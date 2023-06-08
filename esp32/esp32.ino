6ae969725bc60c1047106134b4f9f99e234b07d9
Nik <nl621@ic.ac.uk>
2023-06-08 14:54:57 +0100

    Printing Ben's hex dump with leading 0sgit add .

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -3,14 +3,13 @@
 //MACROS
 const int8_t tx = 17;
 const int8_t rx = 16;
-const String bt_name = "TestRig";
+const uint8_t baudrate = 115200;
 const uint8_t nios_serialport = 2;
 
 HardwareSerial SerialPortNios(nios_serialport);  //if using UART2
 
 void setup() {
 
- 
   //SerialBT.begin();
   Serial.begin(115200);
   //Serial.setTimeout(100);
@@ -19,21 +18,22 @@ void setup() {
 
   SerialPortNios.setTimeout(10);
   delay(1000);
+
 }
 
-uint32_t client_output;
-String output;
 
 void loop() {
 
-  client_output = 0;
-  // SerialPortNios.readBytes(buffer, 4);
-  // Serial.println(String(buffer, HEX));
-  //SerialPortNios.readString();
-  client_output = (SerialPortNios.read() << 24) | (SerialPortNios.read() << 16) | (SerialPortNios.read() << 8) | SerialPortNios.read();
-  output = String(client_output, HEX);
+  if (SerialPortNios.available()) {
 
-  if (output != "ffffffff") { Serial.print("Reading: "); Serial.println(output);}
+    Serial.print("Reading: ");
+    char tmp[32];
+    uint32_t uart_rx = SerialPortNios.read() | (SerialPortNios.read() << 8) | (SerialPortNios.read() << 16) | (SerialPortNios.read() << 24);
+  
+    sprintf(tmp, "0x%.8X", uart_rx);
+    Serial.println(tmp);
+  }
 
-  delay(10);
+  delay(1);
 }
+
