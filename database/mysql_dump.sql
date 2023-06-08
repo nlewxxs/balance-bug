d3572720b4b4c0401e30af37f384db2d12f0db4d
Benedict Short <benedictnjshort@gmail.com>
2023-06-08 12:55:35 +0100

    updating the database

--- database/mysql_dump.sql
+++ database/mysql_dump.sql
@@ -12,3 +12,21 @@ CREATE table BugInformation (
     `LastSeen` TIMESTAMP NOT NULL,
     PRIMARY KEY (`BugId`)
 ) ENGINE=InnoDB;
+
+CREATE table BeaconCharge (
+    `SessionId`    char(100) NOT NULL,
+    `ChargeStatus`  char(100) NOT NULL,
+    PRIMARY KEY (`SessionId`)
+) ENGINE=InnoDB;
+
+CREATE table BeaconOn (
+    `SessionId`    char(100) NOT NULL,
+    `On`  char(100) NOT NULL,
+    PRIMARY KEY (`SessionId`)
+) ENGINE=InnoDB;
+
+CREATE table BeaconConnected (
+    `SessionId`    char(100) NOT NULL,
+    `LastSeen` TIMESTAMP NOT NULL,
+    PRIMARY KEY (`SessionId`)
+) ENGINE=InnoDB;
