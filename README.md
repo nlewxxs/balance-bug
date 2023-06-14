95c7c21ac236e1cd37706216600294aeedc62c06
Nik <nl621@ic.ac.uk>
2023-06-14 10:02:04 +0100

    Fixed watchdog issues and made framework for no-balance-bug

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ Year 2 EEE/EIE project
 ## Quick Links
 - [Onenote](https://imperiallondon-my.sharepoint.com/:o:/r/personal/jjl221_ic_ac_uk/Documents/Year%202/Engineering%20Project/Yr%202%20Project?d=w9ccd0af0dc6e4be9b2675be7d46dc3fd&csf=1&web=1&e=oAOhcg)
 - [Google Doc](https://docs.google.com/document/d/1ZORAzx5ClWhleDJQ3LpGB9u5QRYEvcmVeUC7WK4pWFc/edit?usp=sharing)
+- [Gantt Chart](https://imperiallondon-my.sharepoint.com/:x:/g/personal/jjl221_ic_ac_uk/EV2vmC09kUBPtqds1BDYE6UBRndgxDqH8xsQIs61bqPvZA?e=Z9P5S6)
 
 ## People away days
 > please keep updated :)
 
 | Jacob | JJ | Luca | Ben | Ahmad | Nik |
 |:--------:|:---------:|:--------:|:---------:|:---------:|:---------:|
-| | 27th to 28th March | | 27th to 28th March | | 20th to 21st March |
+| | 27th to 28th March | | | | 20th to 21st March |
 
 
 
--- no-balance/no-balance.ino
+++ no-balance/no-balance.ino
@@ -4,9 +4,13 @@
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #include <AccelStepper.h>
+#include <BluetoothSerial.h>
+#include "Camera.h"
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
 // ----------------- CORE 0 DEFINITIONS ----------------- //
+
+// BluetoothSerial SerialBT;
 
 // #define ENABLE_HTTP_SERVER
 #define OUTPUT_DEBUG
@@ -28,12 +32,16 @@ unsigned long timerDelay = 5000;
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
 // ----------------- CORE 1 DEFINITIONS ----------------- //
 
+float HEADING_SETPOINT = 0;
+float POSITION_SETPOINT = 0;
+
 // Define pin connections
-const int leftDirPin = 32;
-const int leftStepPin = 33;
+const int leftDirPin = 32; //A4
+const int leftStepPin = 33; //A3
 // Define pin connections
-const int rightDirPin = 25;
-const int rightStepPin = 26;
+const int rightDirPin = 25; //A2
+const int rightStepPin = 26; //A1
+//D2 D3 for 23 and 22 (sda and scl)
 
 // Define motor interface type
 #define motorInterfaceType 1
@@ -52,31 +60,64 @@ uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 Quaternion q;           // [w, x, y, z]         quaternion container
 VectorFloat gravity;    // [x, y, z]            gravity vector
+VectorInt16 gyro;
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// heading PD controller
+float Kp_heading = 30; 
+float Ki_heading = 0;
+float Kd_heading = 0; 
+
+float Kp_position = 0;
+float Ki_position = 0;
+float Kd_position = 0;
+
+int maxSpeed = 1000;
+int acceleration = 600;
+
+float Kp_tilt = 0;
+float Ki_tilt = 0;
+float Kd_tilt = 0;
+
+float balanceCenter = 0;  // whatever tilt value is balanced
+float P_bias, T_bias, Trate_bias, H_bias = 0;
+
+float H_derivative, H_out, P_derivative, P_out, T_derivative, T_out, Trate_derivative, Trate_out;
+float H_error[2], P_error[2], T_error[2], Trate_error[2] = { 0, 0 };            
+float H_integral[2], P_integral[2], T_integral[2], Trate_integral[2] = { 0, 0 };  
+float iteration_time;
+unsigned long oldMillis = 0;
 
 float leftWheelDrive;
 float rightWheelDrive;
 
-bool atNode = 0;
+float totalDistance;
+float leftDistance;
+float rightDistance;
 
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< //
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< /
 
 void setup() {
 
   Serial.begin(115200);
-  while (!Serial);    // hang until serial connection established
 
   Wire.begin();
   Wire.setClock(400000); // 400kHz I2C clock
 
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
   // initialize device
+  setColour(170, 0, 255); // purple
+
   mpu.initialize();
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
   // load and configure the DMP
   Serial.println(F("Initializing DMP..."));
-  setColour(170, 0, 255); // purple
+
   devStatus = mpu.dmpInitialize();
 
   // mpu offsets
@@ -96,19 +137,17 @@ void setup() {
       mpu.setDMPEnabled(true);
       dmpReady = true;
       setColour(0, 255, 0); // bueno, set green 
-      delay(1000);
+      delay(600);
       setColour(0, 0, 0); // kill
 
   } else {
       // ERROR!
-      // 1 = initial memory load failed
-      // 2 = DMP configuration updates failed
-      // (if it's going to break, usually the code will be 1)
-      Serial.print(F("DMP Initialization failed (code "));
-      Serial.print(devStatus);
-      Serial.println(F(")"));
-      setColour(255, 0, 0);
-      while (1); // hang
+
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+    setColour(255, 0, 0);
+
   }
 
   //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
@@ -121,17 +160,25 @@ void setup() {
     &communication,    // Task handle to keep track of created task 
     0);                // pin task to core 0 -- by default we pin to core 1 
   
-	leftStepper.setMaxSpeed(200);
-	leftStepper.setAcceleration(1000);
-  leftStepper.setSpeed(200);
-	rightStepper.setMaxSpeed(200);
-	rightStepper.setAcceleration(1000);
-  rightStepper.setSpeed(200);
+	leftStepper.setMaxSpeed(maxSpeed);
+	leftStepper.setAcceleration(acceleration);
+	rightStepper.setMaxSpeed(maxSpeed);
+	rightStepper.setAcceleration(acceleration);
+
+  leftStepper.setSpeed(-2000);
+  rightStepper.setSpeed(2000);
   
   delay(1000);
 }
 
 void loop() {
+
+  // while (!dmpReady) {
+  //   setColour(170, 0, 255);
+  //   delay(500);
+  //   setColour(0, 0, 0);
+  //   delay(500);
+  // }  // hang program if programming did not work
 
   // -------- READ FROM IMU ---------- // 
 
@@ -139,28 +186,84 @@ void loop() {
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    convertYaw(&ypr);
+    mpu.dmpGetGyro(&gyro, fifoBuffer);
   }
 
-	// -------------------------- DRIVE???? ---------------------------- //
+  // leftStepper.setSpeed(300);
+  // leftStepper.run();
 
-  if ((20 < ypr[0]) && (ypr[0] < 100)){
-    leftStepper.moveTo(leftStepper.currentPosition() + 50);
-    rightStepper.moveTo(rightStepper.currentPosition() + 100);
-  } else if ((260 < ypr[0]) && (ypr[0] < 340)) {
-    leftStepper.moveTo(leftStepper.currentPosition() + 100);
-    rightStepper.moveTo(rightStepper.currentPosition() + 50);
+  if (ypr[1] > 0) {
+    leftStepper.setSpeed(-1000);
+    rightStepper.setSpeed(1000);
   } else {
-    leftStepper.moveTo(leftStepper.currentPosition() + 100);
-    rightStepper.moveTo(rightStepper.currentPosition() + 100);
+    leftStepper.setSpeed(1000);
+    rightStepper.setSpeed(-1000);
   }
 
-  leftStepper.run();
-  rightStepper.run();
+  leftStepper.runSpeed();
+  rightStepper.runSpeed();
 
-  // ----------------------------------------------------------------- //
 
-  delayMicroseconds(1000);
+  // float iteration_time = (millis() - oldMillis) / 1000.0;
+  // oldMillis = millis();
+
+  // // -------- PID CONTROLLER ---------- // 
+
+  // // position control first
+  // float positionReading = getPosition();  // example I used for the controller has position read from rotary encoders - this value is total distance travelled
+  // P_error[1] = POSITION_SETPOINT - positionReading;
+  // P_integral[1] = P_integral[0] + P_error[1] * iteration_time;  // 1 is current, 0 is old
+  // P_derivative = (P_error[1] - P_error[0]) / iteration_time;
+  // P_out = Kp_position * P_error[1] + Ki_position * P_integral[1] + Kd_position * P_derivative + P_bias;
+  // // make this output positive to move forwards, negative to move backwards
+
+  // P_integral[0] = P_integral[1];  // time shift integral readings after out calculated
+  // P_error[0] = P_error[1];
+
+  // // balance control
+  // float tiltReading = ypr[1] * 180/M_PI;
+  // T_error[1] = balanceCenter + P_out - tiltReading;             // offset the tilt reading with the distance output to allow us to manipulate the position of the robot via tilt
+  // T_integral[1] = T_integral[0] + T_error[1] * iteration_time;  // 1 is current, 0 is old
+  // T_derivative = (T_error[1] - T_error[0]) / iteration_time;
+  // T_out = Kp_tilt * T_error[1] + Ki_tilt * T_integral[1] + Kd_tilt * T_derivative + T_bias;
+
+  // T_integral[0] = T_integral[1];  // time shift integral readings after out calculated
+  // T_error[0] = T_error[1];
+
+  // // heading control to offset the wheels for rotation
+  // float headingReading = ypr[0] * 180/M_PI;
+  // H_error[1] = HEADING_SETPOINT - headingReading;
+  // H_integral[1] = H_integral[0] + H_error[1] * iteration_time;  // 1 is current, 0 is old
+  // H_derivative = (H_error[1] - H_error[0]) / iteration_time;
+  // H_out = Kp_heading * H_error[1] + Ki_heading * H_integral[1] + Kd_heading * H_derivative + H_bias;
+
+  // H_integral[0] = H_integral[1];  // time shift integral readings after out calculated
+  // H_error[0] = H_error[1];
+
+  // leftWheelDrive = T_out + H_out;  // voltage / pwm that will actually drive the wheels
+  // rightWheelDrive = T_out - H_out;
+
+  // leftWheelDrive = constrain(leftWheelDrive, -maxSpeed, maxSpeed);
+  // rightWheelDrive = -constrain(rightWheelDrive, -maxSpeed, maxSpeed);
+  
+	// // Move the motor one step
+  // if (leftWheelDrive > 0) {
+  //   leftStepper.moveTo(leftStepper.currentPosition() + 20000);
+  // } else {
+  //   leftStepper.moveTo(leftStepper.currentPosition() - 20000);
+  // }
+  // leftStepper.setSpeed(leftWheelDrive);
+	// leftStepper.run();
+
+  // if (rightWheelDrive > 0) {
+  //   rightStepper.moveTo(rightStepper.currentPosition() + 20000);
+  // } else {
+  //   rightStepper.moveTo(rightStepper.currentPosition() - 20000);
+  // }
+  // rightStepper.setSpeed(rightWheelDrive);
+	// rightStepper.run();
+
+  vTaskDelay(10);
 }
 
 void setColour(uint8_t r, uint8_t g, uint8_t b){
@@ -169,87 +272,90 @@ void setColour(uint8_t r, uint8_t g, uint8_t b){
   analogWrite(bluePin, b);
 }
 
-void communicationCode(void* pvParameters) {
-  // -------- OUTPUTS ---------- //
-  for (;;) {
-    Serial.print("L: ");
-    Serial.print(leftWheelDrive);
-    Serial.print(" R: ");
-    Serial.print(rightWheelDrive);
-    Serial.print("  pitch: ");
-    Serial.print(ypr[1] * 180/M_PI);
-    Serial.print(" roll: ");
-    Serial.print(ypr[2] * 180/M_PI);
-    Serial.print(" yaw: ");
-    Serial.println(ypr[0] * 180/M_PI);
-    vTaskDelay(50);
-  }
-}
+// float getPosition() {  //unsure of how this reading will work - needs to be 1D (as in just x)
+//   // could potentially have position just be a relative thing i.e. move forwards 1 / backwards 1 rather than move to position 23?
 
-void convertYaw(float *ypr){
-  while (*ypr[0] < 0) {
-    *ypr[0] += 360;
-  } 
-  while (*ypr[0] > 360) {
-    *ypr[0] -= 360;
-  }
-}
-
-// void communicationCode(void* pvParameters) {
-//   // Serial.println(xPortGetCoreID());
-
-//   #ifdef ENABLE_HTTP_SERVER
-//     wifi setup
-//     WiFi.begin(ssid, password);
-//     Serial.println("Connecting");
-
-//     while (WiFi.status() != WL_CONNECTED) {
-//       delay(500);
-//       Serial.print("Attempting WiFi connection...");
-//     }
-
-//     Serial.println("");
-//     Serial.print("Connected to WiFi network with IP Address: ");
-//     Serial.println(WiFi.localIP());
-//     Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
-//   #endif
-
-//     // looping code - this takes up entirety of cpu time along with controller so NEEDS the delay to allow idle tasks to execute
-//   for (;;) {
-//     #ifdef ENABLE_HTTP_SERVER
-//       if (millis() - lastTime > 5000) {
-//         //Check WiFi connection status
-//         if (WiFi.status() == WL_CONNECTED) {
-//           // WiFiClient client;
-//           HTTPClient http;
-//           // String serverPath = serverName + "/Nodes/Add?SessionId=1&NodeId=3&XCoord=72&YCoord=56";
-//           String serverPath = serverName + "/Edges/Add?SessionId=1&NodeId=2&EdgeNodeId=3&Distance=34.2&Angle=72.0";
-//           // Serial.println(serverPath);
-//           // Your Domain name with URL path or IP address with path
-//           http.begin(serverPath.c_str());
-//           // HTTP GET request
-//           int httpResponseCode = http.GET();
-
-//           if (httpResponseCode > 0) {
-//             Serial.print("HTTP Response code: ");
-//             Serial.println(httpResponseCode);  // HTTP response code e.g. 200
-//             String payload = http.getString();
-//             Serial.println(payload);  // HTTP response package e..g JSON object
-//           } else {
-//             Serial.print("Error code: ");
-//             Serial.println(httpResponseCode);
-//           }
-
-//           // Free resources
-//           http.end();
-//         } else {
-//           Serial.println("WiFi Disconnected");
-//         }
-//         // this delay is not actually necessary as the time waiting for http request is enough for idle tasks to run ?
-//         // vTaskDelay(5000); //delay important to allow idle tasks to execute else processor reboots
-//         lastTime = millis();
-//       }
-//     #endif
-//     vTaskDelay(20);
-//   }
+//   leftDistance = leftStepper.currentPosition()/200.0 * 0.175 * M_PI;
+//   rightDistance = rightStepper.currentPosition()/200.0 * 0.175 * M_PI;
+  
+//   totalDistance = (leftDistance + rightDistance) / 2.0;
+//   return totalDistance;
 // }
+
+float convertYaw(float yaw){
+  while (yaw < 0.0){
+    yaw = yaw + 360.0;
+  }
+  while (yaw > 360.0){
+    yaw = yaw - 360.0;
+  }
+  return yaw;
+}
+
+void communicationCode(void* pvParameters) {
+  // Serial.println(xPortGetCoreID());
+
+  #ifdef ENABLE_HTTP_SERVER
+    wifi setup
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting");
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print("Attempting WiFi connection...");
+    }
+
+    Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+  #endif
+
+  // looping code - this takes up entirety of cpu time along with controller so NEEDS the delay to allow idle tasks to execute
+  for (;;) {
+
+    // -------- OUTPUTS ---------- //
+    Serial.print("Yaw: ");
+    Serial.print(ypr[0]);
+    Serial.print("\t L: ");
+    Serial.print(leftWheelDrive);
+    Serial.print("\t R: ");
+    Serial.println(rightWheelDrive);
+
+    #ifdef ENABLE_HTTP_SERVER
+      if (millis() - lastTime > 5000) {
+        //Check WiFi connection status
+        if (WiFi.status() == WL_CONNECTED) {
+          // WiFiClient client;
+          HTTPClient http;
+          // String serverPath = serverName + "/Nodes/Add?SessionId=1&NodeId=3&XCoord=72&YCoord=56";
+          String serverPath = serverName + "/Edges/Add?SessionId=1&NodeId=2&EdgeNodeId=3&Distance=34.2&Angle=72.0";
+          // Serial.println(serverPath);
+          // Your Domain name with URL path or IP address with path
+          http.begin(serverPath.c_str());
+          // HTTP GET request
+          int httpResponseCode = http.GET();
+
+          if (httpResponseCode > 0) {
+            Serial.print("HTTP Response code: ");
+            Serial.println(httpResponseCode);  // HTTP response code e.g. 200
+            String payload = http.getString();
+            Serial.println(payload);  // HTTP response package e..g JSON object
+          } else {
+            Serial.print("Error code: ");
+            Serial.println(httpResponseCode);
+          }
+
+          // Free resources
+          http.end();
+        } else {
+          Serial.println("WiFi Disconnected");
+        }
+        // this delay is not actually necessary as the time waiting for http request is enough for idle tasks to run ?
+        // vTaskDelay(5000); //delay important to allow idle tasks to execute else processor reboots
+        lastTime = millis();
+      }
+    #endif
+    vTaskDelay(100);
+  }
+}
