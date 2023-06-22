b2eac6b22f6feebc6bb3f35253673363a6a3689d
Benedict Short <benedictnjshort@gmail.com>
2023-06-23 00:39:05 +0100

    added FPGA files used in demo

---
+++ software_dir/software/mazerunner/obj/default/mipi_bridge_config.d
@@ -0,0 +1,73 @@
+obj/default/mipi_bridge_config.o: mipi_bridge_config.c I2C_core.h \
+ ../mazerunner_bsp//HAL/inc/alt_types.h terasic_includes.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_flash.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_flash_dev.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_llist.h \
+ ../mazerunner_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_cache.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../mazerunner_bsp//HAL/inc/io.h ../mazerunner_bsp//HAL/inc/alt_types.h \
+ ../mazerunner_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_irq.h \
+ ../mazerunner_bsp//HAL/inc/nios2.h ../mazerunner_bsp/system.h \
+ ../mazerunner_bsp/linker.h ../mazerunner_bsp//HAL/inc/sys/alt_alarm.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_llist.h \
+ ../mazerunner_bsp//HAL/inc/priv/alt_alarm.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_timestamp.h \
+ ../mazerunner_bsp//drivers/inc/altera_avalon_timer.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_dev.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_warning.h \
+ ../mazerunner_bsp//HAL/inc/sys/alt_stdio.h mipi_bridge_config.h
+
+I2C_core.h:
+
+../mazerunner_bsp//HAL/inc/alt_types.h:
+
+terasic_includes.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_flash.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_flash_dev.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_llist.h:
+
+../mazerunner_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_cache.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../mazerunner_bsp//HAL/inc/io.h:
+
+../mazerunner_bsp//HAL/inc/alt_types.h:
+
+../mazerunner_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_irq.h:
+
+../mazerunner_bsp//HAL/inc/nios2.h:
+
+../mazerunner_bsp/system.h:
+
+../mazerunner_bsp/linker.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_alarm.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_llist.h:
+
+../mazerunner_bsp//HAL/inc/priv/alt_alarm.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../mazerunner_bsp//drivers/inc/altera_avalon_timer.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_dev.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_warning.h:
+
+../mazerunner_bsp//HAL/inc/sys/alt_stdio.h:
+
+mipi_bridge_config.h:
